e36fe55758a5f4179292c975af8a4d1b75521dea
Shaun Meyer <shaun.meyer@dwsconsultants.com>
2017-06-13 20:50:30 +0200

    include throw in rxjs

--- src/app/rxjs-extensions.ts
+++ src/app/rxjs-extensions.ts
@@ -1,4 +1,6 @@
 import 'rxjs/add/observable/interval';
 import 'rxjs/observable/IntervalObservable';
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+
